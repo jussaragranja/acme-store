5b5adc8b842b56ad36bafcbbd80c64e513b23309
Jussara Granja <jussaragranja19@gmail.com>
2023-08-28 11:41:58 -0300

    remove swagger

--- acme-store/card-service/src/main/java/br/com/acme/ApplicationCardService.java
+++ acme-store/card-service/src/main/java/br/com/acme/ApplicationCardService.java
@@ -1,23 +1,10 @@
 package br.com.acme;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@OpenAPIDefinition(
-        info = @Info(title = "Card-Service",
-                version = "1.0.0",
-                description = "Esse projeto está sendo desenvolvido para a disciplina de arquitetura e design de software do MBA Full Stack Web Development - UNIESP",
-                contact = @Contact(
-                        name = "Jussara Souza",
-                        url = "https://www.linkedin.com/in/jussaragranja"
-                )
-        )
-)
 public class ApplicationCardService {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationCardService.class, args);
